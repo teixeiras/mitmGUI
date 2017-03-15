d8e6a3c426c3c1bd50b5d6b688e7ab08eedb8697
Filipe Teixeira <filipe.teixeira@wit-software.com>
2017-03-15 18:50:30 +0000

    %new API

--- src/Mitmgui/Managers/AppManager.java
+++ src/Mitmgui/Managers/AppManager.java
@@ -9,6 +9,7 @@ import javax.imageio.ImageIO;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -19,6 +20,7 @@ import static com.sun.xml.internal.ws.spi.db.BindingContextFactory.LOGGER;
  * Created by teixeiras on 14/03/2017.
  */
 public class AppManager {
+    static File file = null;
 
     public static Image getQRCode(String text) {
         ByteArrayOutputStream out = new ByteArrayOutputStream();
@@ -26,6 +28,19 @@ public class AppManager {
 
         ByteArrayInputStream in = new ByteArrayInputStream(out.toByteArray());
         return new javafx.scene.image.Image(in);
+    }
+
+    public static File homeDirectory () {
+        if (file != null) {
+            return file;
+        }
+        String home = System.getProperty("user.home");
+        file = new File(home, "mitmgui");
+
+        if (!file.exists()) {
+            file.mkdir();
+        }
+        return file;
     }
 
 }
--- src/Mitmgui/Network/FetchAPI.java
+++ src/Mitmgui/Network/FetchAPI.java
@@ -1,70 +1,170 @@
 package Mitmgui.Network;
 
+import Mitmgui.Managers.AppManager;
+import Mitmgui.Managers.PropertiesManager;
 import Mitmgui.Models.Flows.FlowModel;
+import org.pmw.tinylog.Logger;
+
+import java.awt.*;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * Created by teixeiras on 12/03/2017.
  */
 public class FetchAPI {
 
-    public void resume(FlowModel flow) {
-        return dispatch => fetchApi(`/flows/${flow.id}/resume`, { method: 'POST' })
+    private HttpURLConnection connection(String partialUrl) throws IOException {
+        try {
+
+            HttpURLConnection connection = (HttpURLConnection)(new URL("http://"+ PropertiesManager.shared.getURL()+partialUrl).openConnection());
+
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Connection", "Keep-Alive");
+            connection.setRequestProperty("Cookie", CookiesManager.validCookie());
+            connection.connect();
+            return connection;
+
+        } catch (IOException e) {
+            Logger.error(e);
+            throw e;
+        }
     }
 
-    public void resumeAll() {
-        return dispatch => fetchApi('/flows/resume', { method: 'POST' })
+    public void GET(String url) throws IOException {
+        HttpURLConnection connection = this.connection(url);
+
     }
 
-    public void  kill(FlowModel flow) {
-        return dispatch => fetchApi(`/flows/${flow.id}/kill`, { method: 'POST' })
+    public void POST(String url) throws IOException {
+        HttpURLConnection connection = this.connection(url);
+
     }
 
-    public void  killAll() {
-        return dispatch => fetchApi('/flows/kill', { method: 'POST' })
-    }
+    public void PUT(String url,  byte[] data) throws IOException {
+        HttpURLConnection connection = this.connection(url);
 
-
-    public void  remove(FlowModel flow) {
-        return dispatch => fetchApi(`/flows/${flow.id}`, { method: 'DELETE' })
-    }
-
-    public void duplicate(FlowModel flow) {
-        return dispatch => fetchApi(`/flows/${flow.id}/duplicate`, { method: 'POST' })
-    }
-
-    public void replay(FlowModel flow) {
-        return dispatch => fetchApi(`/flows/${flow.id}/replay`, { method: 'POST' })
-    }
-
-    public void  revert(FlowModel flow) {
-        return dispatch => fetchApi(`/flows/${flow.id}/revert`, { method: 'POST' })
-    }
-
-    public void  update(FlowModel flow, data) {
-        return dispatch => fetchApi.put(`/flows/${flow.id}`, data)
-    }
-
-    public void  uploadContent(FlowModel flow, file, type) {
-    const body = new FormData()
-        file       = new window.Blob([file], { type: 'plain/text' })
-        body.append('file', file)
-        return dispatch => fetchApi(`/flows/${flow.id}/${type}/content`, { method: 'post', body })
     }
 
 
-    public void  clear() {
-        return dispatch => fetchApi('/clear', { method: 'POST' })
+    public void resume(FlowModel flow) throws IOException {
+        POST("/flows/"+flow.getId()+"/resume");
     }
 
-    public void  download() {
-        window.location = '/flows/dump'
-        return { type: REQUEST_ACTION }
+    public void resumeAll() throws IOException {
+        POST("/flows/resume");
     }
 
-    public void  upload(file) {
-    const body = new FormData()
-        body.append('file', file)
-        return dispatch => fetchApi('/flows/dump', { method: 'post', body })
+    public void  kill(FlowModel flow) throws IOException {
+        POST("/flows/"+flow.getId()+"/kill");
+    }
+
+    public void  killAll() throws IOException {
+        POST("/flows/kill");
+    }
+
+
+    public void  remove(FlowModel flow) throws IOException {
+        POST("/flows/"+flow.getId());
+    }
+
+    public void duplicate(FlowModel flow) throws IOException {
+        POST("/flows/"+flow.getId()+"/duplicate");
+    }
+
+    public void replay(FlowModel flow) throws IOException {
+        POST("/flows/"+flow.getId()+"/replay");
+    }
+
+    public void  revert(FlowModel flow) throws IOException {
+        POST("/flows/"+flow.getId()+"/revert");
+    }
+
+    public void  update(FlowModel flow,  byte[] data) throws IOException {
+        PUT("/flows/"+flow.getId(), data);
+    }
+
+    public void uploadFile(URL url, byte[] postData) throws IOException {
+        try{
+            int    postDataLength = postData.length;
+
+            HttpURLConnection conn= (HttpURLConnection) url.openConnection();
+            conn.setDoOutput( true );
+            conn.setInstanceFollowRedirects( false );
+            conn.setRequestMethod( "POST" );
+            conn.setRequestProperty("Cookie", CookiesManager.validCookie());
+
+            conn.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded");
+            conn.setRequestProperty( "charset", "utf-8");
+            conn.setRequestProperty( "Content-Length", Integer.toString( postDataLength ));
+            conn.setUseCaches( false );
+            try( DataOutputStream wr = new DataOutputStream( conn.getOutputStream())) {
+                wr.write( postData );
+            }
+        }catch (Exception e) {
+            Logger.error(e);
+            throw e;
+        }
+    }
+    public void  uploadContent(FlowModel flow, byte[] postData, String type) throws Exception{
+        URL url = new URL("http://"+ PropertiesManager.shared.getURL()+
+                "/flows/"+flow.getId()+"/"+type+"/content");
+        uploadFile(url, postData);
+   }
+
+
+    public void  clear() throws IOException {
+        POST("/clear");
+    }
+
+    public void  downloadDump() throws IOException {
+
+        try{
+            HttpURLConnection connection = this.connection("/flows/dump");
+            File file = new File(AppManager.homeDirectory(), "flow.dump");
+            FileOutputStream fop = new FileOutputStream(file);
+
+            // if file doesnt exists, then create it
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            InputStream is = null;
+            is = connection.getInputStream();
+            byte[] byteChunk = new byte[4096]; // Or whatever size you want to read in at a time.
+            int n;
+            while ( (n = is.read(byteChunk)) > 0 ) {
+                baos.write(byteChunk, 0, n);
+            }
+
+
+            fop.write(baos.toByteArray());
+            fop.flush();
+            fop.close();
+            if (Desktop.isDesktopSupported()) {
+                Desktop.getDesktop().open(new File(file.getParent()));
+            }
+
+
+        } catch(Exception e) {
+            Logger.error(e);
+            throw e;
+        }
+
+    }
+
+    public void  uploadDump(File file) throws Exception {
+        URL url = new URL("http://" + PropertiesManager.shared.getURL() + "/flows/dump");
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        InputStream is = new FileInputStream(file);
+        byte[] byteChunk = new byte[4096]; // Or whatever size you want to read in at a time.
+        int n;
+        while ((n = is.read(byteChunk)) > 0) {
+            baos.write(byteChunk, 0, n);
+        }
+
+        uploadFile(url, baos.toByteArray());
     }
 
 }
